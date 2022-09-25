24070329722312968a3a9d2c8e2a62fc3459e300
100 Worries <davecmcdowell@gmail.com>
2022-09-25 16:23:02 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 let moxxi = function() {
     let isDarkMode = true;
 
+    let darkModeToggleBtn = document.querySelector('#toggle-darkmode');
+
     window.addEventListener('DOMContentLoaded', init);
 
     function init() {
@@ -17,18 +19,32 @@ let moxxi = function() {
     }
 
     function bindDarkModeToggle() {
-        let darkModeToggleBtn = document.querySelector('#toggle-darkmode');
         darkModeToggleBtn.addEventListener('click', toggleDarkMode, true);
     }
     
     function toggleDarkMode() {
         isDarkMode = !isDarkMode;
+        (isDarkMode) ? setDarkMode() : setLightMode();
+    }
 
-        if(isDarkMode) {
-            document.querySelector('html').classList.remove('light-theme');
-            document.querySelector('html').classList.add('dark-theme');
-        } else {
-            document.querySelector('html').classList.add('light-theme');
-        }
+    function setDarkMode() {
+        document.querySelector('html').classList.remove('light-theme');
+        document.querySelector('html').classList.add('dark-theme');
+
+        darkModeToggleBtn.querySelector('i').classList.remove('bi-sun');
+        darkModeToggleBtn.querySelector('i').classList.add('bi-moon');
+
+        darkModeToggleBtn.setAttribute('title', 'Light Mode');
+        darkModeToggleBtn.setAttribute('aria-label', 'Light Mode');
+    }
+
+    function setLightMode() {
+        document.querySelector('html').classList.add('light-theme'); 
+
+        darkModeToggleBtn.querySelector('i').classList.remove('bi-moon');
+        darkModeToggleBtn.querySelector('i').classList.add('bi-sun');
+
+        darkModeToggleBtn.setAttribute('title', 'Dark Mode');
+        darkModeToggleBtn.setAttribute('aria-label', 'Dark Mode');
     }
 }();
