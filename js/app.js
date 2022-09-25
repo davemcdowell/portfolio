9fc96f3369f109aa5a60648b290f6e21a51643ac
100 Worries <davecmcdowell@gmail.com>
2022-09-25 16:11:41 -0700

    js setup

---
+++ js/app.js
@@ -0,0 +1,33 @@
+let moxxi = function() {
+    let isDarkMode = true;
+
+    window.addEventListener('DOMContentLoaded', init);
+
+    function init() {
+        console.log('Moxxi here!');
+        bindDarkModeToggle();
+        setToolTips();
+    }
+
+    function setToolTips() {
+        let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl);
+        });
+    }
+
+    function bindDarkModeToggle() {
+        let darkModeToggleBtn = document.querySelector('#toggle-darkmode');
+        darkModeToggleBtn.addEventListener('click', toggleDarkMode, true);
+    }
+    
+    function toggleDarkMode() {
+        isDarkMode = !isDarkMode;
+
+        if(isDarkMode) {
+            document.querySelector('html').classList.add('dark-theme');
+        } else {
+            document.querySelector('html').classList.add('light-theme');
+        }
+    }
+}();
