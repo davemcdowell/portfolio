593968d2f64a84a471d3b3b4a4b8269b6ef90471
100 Worries <davecmcdowell@gmail.com>
2022-09-27 23:15:47 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -55,22 +55,25 @@ let app = function() {
 
         let nameGroup = form.querySelector('#form-name-group');
         let emailGroup = form.querySelector('#form-email-group');
+        let msgGroup = form.querySelector('#form-msg-group');
         let humanGroup = form.querySelector('#form-human-group');
         let fGroups = [nameGroup, emailGroup, humanGroup];
 
         let nameInput = nameGroup.querySelector('input[type="text"]');
         let emailInput = emailGroup.querySelector('input[type="email"]');
+        let msgInput = msgGroup.querySelector('textarea');
         let humanInput = humanGroup.querySelector('input[type="text"]');
 
         let submitBtn = form.querySelector('#form-submit');
         let responseDiv = form.querySelector('#form-response');
 
         nameInput.addEventListener('change', () => {
+            /*
             let name = nameInput.value;
             let validFeedback = nameGroup.querySelector('.valid-feedback');
             let invalidFeedback = nameGroup.querySelector('.invalid-feedback');
 
-            if(isNameValid(name)) {
+            if(isStringValid(name)) {
                 invalidFeedback.style.display = "none";
                 validFeedback.style.display = "block";
                 nameInput.style.borderColor = "#198754";
@@ -82,6 +85,36 @@ let app = function() {
                 nameInput.style.borderColor = "#dc3545;";
 
                 nameGroup.removeAttribute('is-valid', "");
+            }
+            */
+            setFormGroupValidation(nameGroup, nameInput, isStringValid2);
+
+            if(isFormValid(fGroups)) {
+                submitBtn.removeAttribute('disabled');
+                submitBtn.textContent = 'Send';
+            } else {
+                submitBtn.setAttribute('disabled', '');
+                submitBtn.textContent = 'Write Me';
+            }
+        });
+
+        msgInput.addEventListener('change', () => {
+            let msg = msgInput.value;
+            let validFeedback = msgGroup.querySelector('.valid-feedback');
+            let invalidFeedback = msgGroup.querySelector('.invalid-feedback');
+
+            if(isStringValid(msg)) {
+                invalidFeedback.style.display = "none";
+                validFeedback.style.display = "block";
+                msgInput.style.borderColor = "#198754";
+                
+                msgGroup.setAttribute('is-valid', "");
+            } else {
+                validFeedback.style.display = "none";
+                invalidFeedback.style.display = "block";
+                msgInput.style.borderColor = "#dc3545;";
+
+                msgGroup.removeAttribute('is-valid', "");
             }
 
             if(isFormValid(fGroups)) {
@@ -150,14 +183,36 @@ let app = function() {
         });
     }
 
+    function setFormGroupValidation(group, input, validationCheck) {
+        let inputValue = input.value;
+        let validFeedback = group.querySelector('.valid-feedback');
+        let invalidFeedback = group.querySelector('.invalid-feedback');
+
+        if(validationCheck(inputValue)) {
+            invalidFeedback.style.display = "none";
+            validFeedback.style.display = "block";
+            input.style.borderColor = "#198754";
+
+            group.setAttribute('is-valid', "");
+        } else {
+            validFeedback.style.display = "none";
+            invalidFeedback.style.display = "block";
+            input.style.borderColor = "#dc3545;";
+
+            group.removeAttribute('is-valid');
+        }         
+    }
+
     function isEmailValid(email) {
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
     }
 
-    function isNameValid(name) {
+    function isStringValid(name) {
         return (name !== "") ? true : false;
     }
+
+    const isStringValid2 = (string) => { return Boolean(string !== ""); }
 
     function isHumanValid(human) {
         return (parseInt(human) === 7) ? true : false;
