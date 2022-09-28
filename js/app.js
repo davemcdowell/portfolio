18f20f720888dd7d28b549e46ac02446a59c464a
100 Worries <davecmcdowell@gmail.com>
2022-09-28 16:14:43 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,6 +13,17 @@ let app = function() {
         bindDarkModeToggle();
         setContactForm();
         setToolTips();
+
+        document.addEventListener( 'DOMContentLoaded', function () {
+            new Splide( '#card-carousel', {
+                  perPage    : 2,
+                  breakpoints: {
+                      640: {
+                          perPage: 1,
+                      },
+                  },
+            } ).mount();
+          } );
     }
 
     function setToolTips() {
