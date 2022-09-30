c4145153c4c52104f7a3ce22a5748d209d256fc4
100 Worries <davecmcdowell@gmail.com>
2022-09-30 11:57:56 -0700

    edits...

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ let app = function() {
         type        : 'loop',
         perPage     : 2,
         perMove     : 1,
-        arrows      : 'slider',
         gap         : '2rem',
         width       : '950px',
         pagination  : false,
