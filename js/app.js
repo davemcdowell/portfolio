fa713b3cd1332881ba5f41d5444d09f3e81b439b
100 Worries <davecmcdowell@gmail.com>
2022-10-03 18:51:59 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -48,7 +48,7 @@ let app = function() {
         document.documentElement.classList.remove('light-theme');
         document.documentElement.classList.add('dark-theme');
 
-        icon.classList.remove('r180');
+        icon.classList.remove('r90');
 
         darkModeToggleBtn.dataset.bsOriginalTitle = 'Light Mode';
         darkModeToggleBtn.setAttribute('title', 'Light Mode');
@@ -61,7 +61,7 @@ let app = function() {
 
         document.documentElement.classList.add('light-theme');
 
-        icon.classList.add('r180');
+        icon.classList.add('r90');
 
         darkModeToggleBtn.dataset.bsOriginalTitle = 'Dark Mode';
         darkModeToggleBtn.setAttribute('title', 'Dark Mode');
