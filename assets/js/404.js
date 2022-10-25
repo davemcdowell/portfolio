1d80ffe46f20bbd40553996e818c764daddfc925
100 Worries <davecmcdowell@gmail.com>
2022-10-24 23:15:19 -0700

    Update 404.js

--- assets/js/404.js
+++ assets/js/404.js
@@ -2,6 +2,7 @@ let fourOFour = function() {
     window.addEventListener('DOMContentLoaded', init);
 
     let blimp = document.querySelector('.blimp');
+    let blimpLoop;
 
     let blimpHits = 0;
     const blimpHitMax = 4;
@@ -16,14 +17,29 @@ let fourOFour = function() {
             return;
 
         blimp.addEventListener('click', blimpHit);
+        blimpLoop = setInterval(moveBlimp, 90);
     }
+    
+    function moveBlimp() {
+        let margin = 0;
+    
+        let l = window.screen.width;
+        let w = 1300;
+
+        console.log(w);
+        if (margin == w) {
+          margin = 0 + "px";
+        } else {
+          blimp.style.marginLeft = margin + "px";
+        }
+        margin += 10;
+      }
 
     function blimpHit() {
         blimpHits++;
 
         if(blimpHits >= blimpHitMax) {
             blimp.remove();
-            console.log('blimp destroyed');
         }
     }
 }();
