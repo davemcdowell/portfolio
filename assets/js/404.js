6567cd56683f4731f3e8efdd4ca258a74ac83c31
100 Worries <davecmcdowell@gmail.com>
2022-10-25 02:37:58 -0700

    Update 404.js

--- assets/js/404.js
+++ assets/js/404.js
@@ -25,7 +25,7 @@ let fourOFour = function() {
     function moveBlimp() {
         let margin = 0;
         margin = (margin > window.innerWidth ? 0 : margin + speed);
-        blimp.style.marginRight = margin + "px";
+        blimp.style.left = margin + "px";
         console.log('moving');
     }
 
