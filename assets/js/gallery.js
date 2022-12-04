db51fce05930b659be58432b8980e8dd1c706516
100 Worries <davecmcdowell@gmail.com>
2022-12-03 21:38:38 -0800

    Update gallery.js

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,6 +1,4 @@
 const gallery = function() {
-    const { Video, Cubemap } = window.splide.Extensions;
-
     window.addEventListener('DOMContentLoaded', init);
 
     function init() {
@@ -38,7 +36,7 @@ const gallery = function() {
         });
 
         mainSlide.sync(thumbnailSlide);
-        mainSlide.mount(window.splide.Extensions);
+        mainSlide.mount({ Video, Cubemap });
         thumbnailSlide.mount();
     }
 
