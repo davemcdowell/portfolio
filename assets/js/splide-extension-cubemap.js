c10f8a39af06454ea14c557b219a26cbac0a7a75
100 Worries <davecmcdowell@gmail.com>
2022-12-03 20:16:10 -0800

    Update splide-extension-cubemap.js

--- assets/js/splide-extension-cubemap.js
+++ assets/js/splide-extension-cubemap.js
@@ -1,5 +1,5 @@
 function Cubemap(Splide, Components) {
-  const { slide } = Components.Slides;
+  const { slide } = Components.Elements;
 
   let _cubemap;
   let _wrapper;
@@ -8,38 +8,42 @@ function Cubemap(Splide, Components) {
   function mount() {
     console.log('cubemap splide registered and mounted!');
     
-    //if(slide.getAttribute('data-splide-cubemap')) {
-      //add an identifier to this slide similar to video extetnsion
-      slide.classList.add('splide__slide--has-cubemap');
+    let _slides = Splide.splides;
 
-      //create the container for the button and wrapper (in that order) - root element for us -
-      _cubemap = document.createElement("div");
-      _cubemap.classList.add('splide__cubemap');
-
-      //create the wrapper for the cubemap display
-      _wrapper = document.createElement("div");
-      _wrapper.style.display = 'none';
-      _wrapper.classList.add('splide__cubemap__wrapper');
-
-      //create our play button
-      _btn = document.createElement("button");
-      _btn.classList.add('splide__cubemap__play');
-      _btn.setAttribute('type', 'button');
-      _btn.setAttribute('aria-label', 'View Cubemap');
-
-      //append play button to cubemap 
-      _cubemap.appendChild(_btn);
-
-      //append wrapper to cubemap 
-      _cubemap.appendChild(_wrapper);
-
-      //append our root to the slide
-      slide.appendChild(_cubemap);
-
-      //play button event listener
-      _btn.addEventListener('click', playCubemap);
+    for(slide in _slides) {
+      if(slide.getAttribute('data-splide-cubemap')) {
+        //add an identifier to this slide similar to video extetnsion
+        slide.classList.add('splide__slide--has-cubemap');
+  
+        //create the container for the button and wrapper (in that order) - root element for us -
+        _cubemap = document.createElement("div");
+        _cubemap.classList.add('splide__cubemap');
+  
+        //create the wrapper for the cubemap display
+        _wrapper = document.createElement("div");
+        _wrapper.style.display = 'none';
+        _wrapper.classList.add('splide__cubemap__wrapper');
+  
+        //create our play button
+        _btn = document.createElement("button");
+        _btn.classList.add('splide__cubemap__play');
+        _btn.setAttribute('type', 'button');
+        _btn.setAttribute('aria-label', 'View Cubemap');
+  
+        //append play button to cubemap 
+        _cubemap.appendChild(_btn);
+  
+        //append wrapper to cubemap 
+        _cubemap.appendChild(_wrapper);
+  
+        //append our root to the slide
+        slide.appendChild(_cubemap);
+  
+        //play button event listener
+        _btn.addEventListener('click', playCubemap);
+      }
     }
-  //}
+  }
 
   function playCubemap() {
     console.log('Play Cubemap');
