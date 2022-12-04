0040c5d99c459597145fc1b40c244f88ed21f881
100 Worries <davecmcdowell@gmail.com>
2022-12-03 18:50:01 -0800

    cubemap_splide extension

---
+++ assets/js/cubemap_splide.js
@@ -0,0 +1,45 @@
+export function CubemapSplide(Splide, Components) {
+  const { slide } = Components.Slides;
+
+  let _cubemap;
+  let _wrapper;
+  let _btn;
+
+  function mount() {
+    
+    console.log('cubemap splide registered and mounted!');
+
+    //add an identifier to this slide similar to video extetnsion
+    slide.classList.add('splide__slide--has-cubemap');
+
+    //create the container for the button and wrapper (in that order) - root element for us -
+    _cubemap = document.createElement("div");
+    _cubemap.classList.add('splide__cubemap');
+
+    //create the wrapper for the cubemap display
+    _wrapper = document.createElement("div");
+    _wrapper.style.display = 'none';
+    _wrapper.classList.add('splide__cubemap__wrapper');
+
+    //create our play button
+    _btn = document.createElement("button");
+    _btn.classList.add('splide__cubemap__play');
+    _btn.setAttribute('type', 'button');
+    _btn.setAttribute('aria-label', 'View Cubemap');
+
+    //play button event listener
+
+    //append play button to cubemap 
+    _cubemap.appendChild(_btn);
+
+    //append wrapper to cubemap 
+    _cubemap.appendChild(_wrapper);
+
+    //append our root to the slide
+    slide.appendChild(_cubemap);
+  }
+
+  return {
+    mount,
+  };
+}
