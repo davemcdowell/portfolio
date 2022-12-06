f8e7c23770c8500fdec29c379c670fae5aa3f896
100 Worries <davecmcdowell@gmail.com>
2022-12-06 11:25:17 -0800

    Update splide-extension-cubemap.js

--- assets/js/splide-extension-cubemap.js
+++ assets/js/splide-extension-cubemap.js
@@ -114,6 +114,8 @@ const Cubemap = function(Splide, Components) {
     _volumeInput.setAttribute('type', 'range');
     _volumeInput.setAttribute('min', 0);
     _volumeInput.setAttribute('max', 1);
+    _volumeInput.setAttribute('step', 0.1);
+    _volumeInput.setAttribute('value', 0.5);
 
     /* fullscreen toggle */ 
     let _fullscreenBtn = document.createElement('button');
