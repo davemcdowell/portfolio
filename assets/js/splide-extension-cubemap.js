7b55a65cad7f891250ebeda2b69ec3a58f5a2d5a
100 Worries <davecmcdowell@gmail.com>
2022-12-05 21:12:09 -0800

    Update splide-extension-cubemap.js

--- assets/js/splide-extension-cubemap.js
+++ assets/js/splide-extension-cubemap.js
@@ -5,7 +5,7 @@ const Cubemap = function(Splide, Components) {
     for(var i = 0; i < slides.length; i++) {
       if(slides[i].getAttribute('data-splide-cubemap')) {
         createCubemapBase(slides[i], i);
-        Splide.slides.getAt(i).on('inactive', disableCubemap);
+        slides.getAt(i).on('inactive', disableCubemap);
       }
     }
     Splide.on('inactive', disableCubemap);
