2cd34a6e06233ad0a8f002042f0fcf4c8bf6bcd4
100 Worries <davecmcdowell@gmail.com>
2022-12-08 14:17:15 -0800

    Update splide-extension-cubemap.js

--- assets/js/splide-extension-cubemap.js
+++ assets/js/splide-extension-cubemap.js
@@ -41,7 +41,7 @@ const Cubemap = function(Splide, Components) {
     //create the wrapper for the cubemap display
     _wrapper = document.createElement("div");
     _wrapper.style.display = 'none';
-    _wrapper.classList.add('splide__cubemap__wrapper', 'no-drag');
+    _wrapper.classList.add('splide__cubemap__wrapper');
 
     //create play button
     _playBtn = document.createElement("button");
