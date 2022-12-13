5b18b23a2181c837fa3269dd5d74ed54d356efc3
100 Worries <davecmcdowell@gmail.com>
2022-12-13 09:35:56 -0800

    Update splide-extension-cubemap.js

--- assets/js/splide-extension-cubemap.js
+++ assets/js/splide-extension-cubemap.js
@@ -132,7 +132,7 @@ const SplideCubemap = function(Splide, Components) {
     _rotSpeedDropup.setAttribute('type', 'button');
     _rotSpeedDropup.setAttribute('data-bs-toggle', 'dropdown');
     _rotSpeedDropup.setAttribute('aria-expanded', 'false');
-    _rotSpeedDropup.setAttribute('data-bs-offset', '56, 0');
+    _rotSpeedDropup.setAttribute('data-bs-offset', '20, 20');
 
     let _speedUL = document.createElement('ul');
     _speedUL.classList.add('dropdown-menu', 'dropdown-menu-end', 'bg-blur', 'border', 'mb-2', 'no-mw');
