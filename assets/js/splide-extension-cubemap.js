bbe3ce167346eed1222cd9007106839faef34a1b
100 Worries <davecmcdowell@gmail.com>
2022-12-17 15:32:13 -0800

    Update splide-extension-cubemap.js

--- assets/js/splide-extension-cubemap.js
+++ assets/js/splide-extension-cubemap.js
@@ -432,7 +432,7 @@ const SplideCubemap = function(Splide, Components) {
       let _useAutoRotate = true;
       let _reqestAnimation;
       */
-     
+
       //this.toggleAutoRotate = function () { _useAutoRotate = !_useAutoRotate; };
       /*
       this.toggleFullscreen = function () { 
@@ -483,7 +483,7 @@ const SplideCubemap = function(Splide, Components) {
     _reqestAnimation;
 
     animate() {
-      if(this._isDisabled)
+      if(_isDisabled)
         return;
 
       this.controls.autoRotate = this._useAutoRotate;
